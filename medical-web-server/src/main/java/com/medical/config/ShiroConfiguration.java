9acbf5ef0561df654c05a83c24e7a1763de58a20
baiyaguang <baiyaguang@finupgrooup.com>
2018-09-05 07:21:18 +0800

    修改无法访问静态资源bug

--- medical-web-server/src/main/java/com/medical/config/ShiroConfiguration.java
+++ medical-web-server/src/main/java/com/medical/config/ShiroConfiguration.java
@@ -10,13 +10,21 @@ import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSource
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.medical.filter.ShiroRealm;
 
+/**
+ * @author apple
+ * @desc shiro 登录验证和权限管理
+ */
 @Configuration
 public class ShiroConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(ShiroConfiguration.class);
 
     /**
      * 将自己实现的验证方式 放到容器里
@@ -37,7 +45,6 @@ public class ShiroConfiguration {
     // Filter工厂，设置对应的过滤条件和跳转条件
     @Bean
     public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
-        System.out.println("ShiroConfiguration.shirFilter()");
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
 
         /**
@@ -58,17 +65,23 @@ public class ShiroConfiguration {
         // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
         filterChainDefinitionMap.put("/logout", "logout");
 
-        // <!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
-        // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
-        filterChainDefinitionMap.put("/**", "authc");
+        /**
+         * 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边
+         * -->:这是一个坑呢，一不小心代码就不好使了;authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+         */
         filterChainDefinitionMap.put("**/static/**", "anon");
-        filterChainDefinitionMap.put("**/templates", "anon");
+        filterChainDefinitionMap.put("/css/**", "anon");
+        filterChainDefinitionMap.put("/image/**", "anon");
+        filterChainDefinitionMap.put("/js/**", "anon");
+
+        filterChainDefinitionMap.put("/templates/**", "anon");
+        filterChainDefinitionMap.put("/**", "authc");
 
         // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
         shiroFilterFactoryBean.setLoginUrl("/login");
         // 登录成功后要跳转的链接
         shiroFilterFactoryBean.setSuccessUrl("/dashboard");
-        // 未授权界面;
+        // 未授权界面
         shiroFilterFactoryBean.setUnauthorizedUrl("/403");
 
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
--- medical-web-server/src/main/java/com/medical/controller/LoginController.java
+++ medical-web-server/src/main/java/com/medical/controller/LoginController.java
@@ -1,21 +1,28 @@
 package com.medical.controller;
 
-import javax.servlet.http.HttpSession;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.StringUtils;
 
 import com.medical.dao.AdminDao;
-import com.medical.domain.Admin;
 
-@RestController
+@Controller
 @RequestMapping
 public class LoginController {
+
+    private static final Logger logger = Logger.getLogger(LoginController.class);
 
     @Autowired
     private AdminDao admindao;
@@ -34,15 +41,33 @@ public class LoginController {
      * @return
      */
     @PostMapping("/login")
-    public ModelAndView loginPost(Admin admin, Model model, HttpSession httpSession) {
-        Admin adminRes = admindao.findByNameAndPassword(admin);
-        if (adminRes != null) {
-            httpSession.setAttribute("admin", adminRes);
-            return new ModelAndView("redirect:dashboard");
-        } else {
-            model.addAttribute("error", "用户名或密码错误，请重新登录！");
-            return new ModelAndView("login");
+    public String loginPost(HttpServletRequest request, Model model, Map<Object, Object> map) {
+
+        // 登录失败从request中获取shiro处理的异常信息。shiroLoginFailure:就是shiro异常类的全类名.
+        Object exception = request.getAttribute("shiroLoginFailure");
+        String msg = "";
+        logger.info(UnknownAccountException.class.getName());
+        if (exception != null) {
+            if (StringUtils.equals(UnknownAccountException.class.getName(), exception)) {
+                msg = "账号不存在";
+            } else if (StringUtils.equals(IncorrectCredentialsException.class.getName(), exception)) {
+                msg = "密码不正确";
+            } else {
+                msg = "未知错误";
+            }
+            model.addAttribute("error", msg);
+
+            return "/login";
         }
+        // Admin adminRes = admindao.findByNameAndPassword(admin);
+        // if (adminRes != null) {
+        // httpSession.setAttribute("admin", adminRes);
+        // return new ModelAndView("redirect:dashboard");
+        // } else {
+        // model.addAttribute("error", "用户名或密码错误，请重新登录！");
+        // return new ModelAndView("login");
+        // }
+        return "/dashboard";
     }
 
     @GetMapping("/register")
